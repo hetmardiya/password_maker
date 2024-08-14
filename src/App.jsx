a6eb3cbed2d5410aaeb3edbb932e54b2780e2621
Het Mardiya <hetmardiya9@gmail.com>
2024-08-14 23:43:04 +0530

    create a new vite project and clean up the code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState , useCallback } from 'react'
+import './App.css'
+
+function App() {
+  let [length , setLength] = useState(8)
+  let [numberAllowed , setNumberAllowed] = useState(false)
+  let [charAllowed , setCharAllowed] = useState(false)
+  let [password , setPassword] = useState("")
+
+  let passwordGenerator = () =>{}
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
